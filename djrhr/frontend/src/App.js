d967ee005eac8d55a885a26684d0121bee16ebe2
DevShasa <shasa.thuo@gmail.com>
2021-12-08 09:17:45 +0300

    initial commit

---
+++ djrhr/frontend/src/App.js
@@ -0,0 +1,21 @@
+
+import React, { Component} from "react";
+
+
+class App extends Component {
+
+    constructor(){
+        super();
+    }
+
+    render(){
+        return(
+            <div>
+                Serving the react index
+            </div>
+        )
+    }
+
+}
+
+export default App;
